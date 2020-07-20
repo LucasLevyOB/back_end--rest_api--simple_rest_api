645f7434ad090b07e4660125483f434c74f9433e
Lucas Levy <oliveiralucaslevy@gmail.com>
2020-07-20 20:22:36 -0300

    Adding post method

--- app/Models/DeleteModel.php
+++ app/Models/DeleteModel.php
@@ -1,7 +1,22 @@
 <?php
 namespace Models;
 
-class DeleteModel
+use Models\ConnectionModel;
+use Models\MethodsHttpModel;
+
+class DeleteModel extends MethodsHttpModel
 {
-    
+    private ConnectionModel $connection;
+    private string $query;
+    private $prepare;
+
+    public function delete(int $receivedId)
+    {
+        $this->connection = new ConnectionModel();
+        $this->query = "DELETE FROM sra_products WHERE pro_id=:pro_id";
+        $this->prepare = $this->connection->connectToDatabase()->prepare($this->query);
+        $this->prepare->bindParam(':pro_id', $receivedId, \PDO::PARAM_INT);
+        $this->prepare->execute();
+        return $this->prepare->rowCount();
+    }
 }
